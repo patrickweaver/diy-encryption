d4366a387623c7dc3e99c8ebf9ed43e87a278c7d
System <none>
2017-05-03 21:25:19 +0000

    Picking primes working on front end for public key

--- public/client.js
+++ public/client.js
@@ -7,25 +7,55 @@
 // add other scripts at the bottom of index.html
 
 $(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(data) {
-    data.dreams.forEach(function(dream) {
-      $('<li>' + dream + '</li>').appendTo('ul#dreams');
-    });
-  });
 
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
-    if (dream.length === 0) {
-      return;
+  var prime1
+  var prime2
+  
+  function clearPrime(clicked, prime){
+    clicked.removeClass(prime);
+    $( "input#" + prime ).val("");
+    return null;
+  }
+  
+  $( "#primes-under-50 li" ).click(function() {
+    var num = parseInt($( this ).html());
+    // If there is no prime1 set number chosen to prime1:
+    if (!prime1) {
+      if (prime2 != num) {
+        prime1 = num;
+        $( this ).addClass("prime1");
+        $( "input#prime1" ).val(num);
+      } else {
+        prime2 = clearPrime($( this ), "prime2");
+      }
+    } else {
+      // If there is a prime1, but not prime2, set number chosen to prime2:
+      if (!prime2) {
+        if (prime1 != num) {
+          prime2 = num;
+          $( this ).addClass("prime2");
+          $( "input#prime2" ).val(num);
+        } else {
+          prime1 = clearPrime($( this ), "prime1");
+        }
+      // If prime1 and prime2 are already selected:
+      } else {
+        // If either is chosen again, reset that prime to null:
+        if (prime1 === num) {
+          prime1 = clearPrime($( this ), "prime1");
+        } else if (prime2 === num) {
+          prime2 = clearPrime($( this ), "prime2");
+        } else {
+          // Do nothing
+        }
+      }
     }
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li>' + dream + '</li>').appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
+    if (prime1 && prime2){
+      $( "form#pick-primes" ).show();
+    } else {
+      $( "form#pick-primes" ).hide();
+    }
+    console.log("Prime1: " + prime1 + ", Prime2: " + prime2);
   });
 
 });
--- server.py
+++ server.py
@@ -3,16 +3,10 @@ import sys
 from flask import Flask, send_from_directory, jsonify, request, render_template
 app = Flask(__name__, static_folder='views')
 
-data = {
-  "dreams": [
-    "2",
-    5,
-    "Hello!"
-  ],
-  "messages": []
-}
 
-
+# - - - - - - - - - - - - - - - - 
+# Offset
+# - - - - - - - - - - - - - - - - 
 
 def offsetEncrypt(myString, myOffset):
   newString = ""
@@ -45,6 +39,10 @@ def offsetDecrypt(myEncodedString, myOffset):
     newChar = chr(newIntC)
     newString += newChar
   return newString
+
+# - - - - - - - - - - - - - - - - 
+# Shared Key
+# - - - - - - - - - - - - - - - - 
 
 def keyEncrypt(myString, myPassword):
   newString = ""
@@ -100,22 +98,158 @@ def keyDecrypt(myEncodedString, myPassword):
       count = 0
   return newString
 
+# - - - - - - - - - - - - - - - - 
+# Public Key
+# - - - - - - - - - - - - - - - - 
+
+from random import randint
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def coprime(a, b):
+    return gcd(a, b) == 1
+  
+primesUnder20 = [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+def generateKeys():
+  
+  print("Pick one of the following prime numbers:")
+  for p in primesUnder20:
+    print(p)
+  print()
+  prime1 = int(input())
+  if prime1 not in primesUnder20:
+    return ["Error", "Not a valid prime number."]
+  print("-- Prime 1: " + str(prime1))
+  print()
+  
+  print("Pick another of the following prime numbers:")
+  for p in primesUnder20:
+    if p != prime1:
+      if p != prime1 - 1:
+        if p != prime1 + 1:
+          print(p)
+  print()
+  prime2 = int(input())
+  if prime2 not in primesUnder20:
+    return ["Error", "Not a valid prime number."]
+  if prime2 == prime1:
+    return ["Error", "Primes chosen are the same"]
+  print("-- Prime 2: " + str(prime2))
+  print()
+  
+  modulus = prime1 * prime2
+  coprimesOf = (prime1 - 1) * (prime2 - 1)
+  print("-- Modulus: " + str(modulus))
+  
+  print()
+  print("Pick one of the following coprimes with " + str(coprimesOf) + " [(" + str(prime1) + " - 1) * (" + str(prime2) + " - 1)]:")
+  validEEs = []
+  for n in range(2, coprimesOf):
+    if coprime(n, coprimesOf):
+      validEEs.append(n)
+      print(n)
+  print()
+  print("Pick one of the above coprimes with " + str(coprimesOf) + " [(" + str(prime1) + " - 1) * (" + str(prime2) + " - 1)]:")
+  print()
+  ee = int(input())
+  if ee in validEEs:
+    print("-- Encryption Exponent: " + str(ee))
+    print()
+    publicKeys = [ee, modulus]
+  else:
+    return ["Error", "Not a valid coprime."]
+  
+  de = 0
+  for n in range(2, coprimesOf):
+    if (n * ee) % coprimesOf == 1:
+      de = n
+      print("-- Decrypt Exponent: " + str(de))
+      privateKeys = [de, modulus]
+      return [publicKeys, privateKeys]
+  if de == 0:
+    return ["Error", "No private decrypt exponent found."]
+  return["Error", "Ended without valid return"]
+
+def charToTildeString(tildeString, nonAsciiInt):
+  if nonAsciiInt > 93:
+    rand = randint(0, 93)
+    tildeString += chr(rand + 32)
+    nonAsciiInt = nonAsciiInt - rand
+    return charToTildeString(tildeString, nonAsciiInt)
+  else:
+    tildeString += chr(nonAsciiInt + 32)
+    tildeString += "~"
+    return tildeString
+  
+
+def privateKeyEncrypt(plainMessage, key):
+  newString = ""
+  count = 0
+  for c in plainMessage:
+    intC = ord(c) - 32
+    if intC > 93:
+      return "Error"
+    if intC < 0:
+      return "Error"
+    newIntC = ((intC**key[0]) % key[1])
+    if newIntC > 93:
+      tildeAdd = charToTildeString("~", newIntC)
+      newString += tildeAdd
+    else: 
+      newChar = chr(newIntC + 32)
+      newString += newChar
+      count += 1
+      if count > 93:
+        count = 0
+  return newString
+  
+def privateKeyDecrypt(encMessage, key):
+  newString = ""
+  tildeBank = 0
+  inTilde = False
+  count = 0
+  for c in encMessage:
+    convChar = False
+    if c == "~":
+      if inTilde:
+        intC = tildeBank
+        tildeBank = 0
+        inTilde = False
+        convChar = True
+      else:
+        inTilde = True
+    else:
+      if inTilde:
+        tildeBank += ord(c) - 32
+      else:
+        intC = ord(c) - 32
+        if intC > 93:
+          return "Error"
+        if intC < 0:
+          return "Error"
+        convChar = True
+    if convChar:
+      newIntC = ((intC**key[0]) % key[1])
+      newChar = chr(newIntC + 32)
+      newString += newChar
+      count +=1
+      if count > 93:
+        count = 0
+  return newString
+
+# - - - - - - - - - - - - - - - - 
+# Routes
+# - - - - - - - - - - - - - - - - 
+
 @app.route("/")
 def hello():
   return app.send_static_file('index.html')
 
-@app.route("/dreams", methods=["GET"])
-def get_dreams():
-  return jsonify(**data)
-
-@app.route("/dreams", methods=["POST"])
-def add_dream():
-  data["dreams"].append(request.args["dream"])
-  return '"OK"'
-
-@app.route("/messages", methods=["GET"])
-def get_messages():
-  return jsonify(**data)
+# Offset:
 
 @app.route("/offset/encrypt")
 def offset_encrypt():
@@ -155,6 +289,8 @@ def offset_decrypt():
 def offset():
   return app.send_static_file("offset.html")
 
+# Shared Key:
+
 @app.route("/shared-key/encrypt")
 def shared_key_encrypt():
   message = request.args.get("message")
@@ -185,10 +321,17 @@ def shared_key_decrypt():
   else:
     return app.send_static_file("offset.html")
 
-
 @app.route("/shared-key")
 def shared_key():
   return app.send_static_file("shared-key.html")
+
+# Public Key:
+
+@app.route("/public-key")
+def public_key():
+  return app.send_static_file("public-key.html")
+
+# Public Directory:
 
 @app.route('/<path:path>')
 def send_static(path):
